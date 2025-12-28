72d96d6e97e3f835e1e44f53e9bb71ce94a7b753
Wajid Ali <wajid.ali@laksol.com>
2025-12-29 04:55:31 +0500

    add services

---
+++ order-service/src/test/java/order_service/order__service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package order_service.order__service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
